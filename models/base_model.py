3fa225e3ad7f3c2517e91e65ddf4382a52dcb0a1
braaabdelmoaty3 <braaabdelmoaty3@gmail.com>
2024-02-11 23:53:21 +0200

    task 3

--- models/base_model.py
+++ models/base_model.py
@@ -5,14 +5,23 @@ from uuid import uuid4
 
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
 
     def save(self):
         """Update the public attribute updated_at to the current date."""
         self.updated_at = datetime.datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()
