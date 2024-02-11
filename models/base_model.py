e6768358037a3b338383d2593768d5beb19fba4f
braaabdelmoaty3 <braaabdelmoaty3@gmail.com>
2024-02-11 23:34:09 +0200

    task 3

--- models/base_model.py
+++ models/base_model.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
 import uuid
-from uuid import uuid4
 import datetime
+from uuid import uuid4
 
-
-# make a class called basemodel (done)
-# then make an attribute called ID use the uuid.uuid4 (done)
 
 class BaseModel:
     def __init__(self):
-        self.format = 
-        self.id = str(uuid4())
-        self.created_at = datetime.datetime()
-        self.updated_at = datetime.datetime()
-    
-    def __str__(self):
-        clname = self.__str__.__name__
-        return f"{clname} {self.id} {self.__dict__}"
-    
-    # make a fun save to save the the public attribute updated_at to the current date 
-    def save(self):
-        """update the public attribute updated_at to the current date. """
-        self.updated_at = datetime.today()
-        #dont forget to store it
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
-   
+    def save(self):
+        """Update the public attribute updated_at to the current date."""
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        return obj_dict
+
+    def __str__(self):
+        clname = self.__class__.__name__
+        return f"{clname} {self.id} {self.__dict__}"
+
